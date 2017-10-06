603dfcadcd37cf0d0af24bf1c5eeea7e7f8a628c
Harris Christiansen (hc15ret2) <harrischristiansen@mac.com>
2017-10-06 01:16:27 -0400

    Add more custom maps

--- base/client/catalog.py
+++ base/client/catalog.py
@@ -2,4 +2,12 @@ GENERALS_MAPS = [
 	"who will win the best",
 	"1v1 Ultimate",
 	"King of the Hill (FFA)",
+	"KILL A KING",
+	"The War of Classes",
+	"Plots",
+	"City",
+	"Speed",
+	"Experiment G",
+	"WIN or LOSE",
+	"Russian Roulette ",
 ]
